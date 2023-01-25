358de4f197b5731dbc8c88bf4e3a0b1fa6fd4964
Dominik Otocan <dominik.otocan1@gmail.com>
2023-01-25 10:44:11 +0100

    fixed the bug

--- app.js
+++ app.js
@@ -22,11 +22,7 @@ async function main() {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-<<<<<<< HEAD
-app.set('view engine', 'jade');
-=======
 app.set('view engine', 'pug');
->>>>>>> a3b421b0c8249b35946c94a162dc62141a87bdef
 
 app.use(logger('dev'));
 app.use(express.json());
